d0242c05549ac4d61a1e0a129bfab8b8ea5c7824
Frikk Brændsrød <fribra05@yahoo.com>
2025-01-29 11:39:12 +0100

    implemented psvm in mian class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Game.java
+++
@@ -1,4 +0,0 @@
-package edu.ntnu.idi.bidata.idatg2003mappe;
-
-public class Game {
-}
--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Main.java
@@ -1,4 +1,9 @@
 package edu.ntnu.idi.bidata.idatg2003mappe;
 
 public class Main {
+
+
+  public static void main(String[] args) {
+
+  }
 }
