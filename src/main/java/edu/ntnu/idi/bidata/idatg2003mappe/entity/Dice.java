ba0dd8f40971d3e318381881cfb0a52691f35da9
Frikk Brændsrød <fribra05@yahoo.com>
2025-01-29 12:52:50 +0100

    Added rollToTurnMarker and javaDoc

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Dice.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Dice.java
@@ -5,10 +5,21 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Dice {
-  int number;
 
+  /**
+   * Return an integer between 1 and 6 (inclusive) to simulate a dice.
+   * @return int
+   */
   public int rollDice(){
-    int randomInt = ThreadLocalRandom.current().nextInt(1, 6 + 1);
-    return randomInt;
+    return ThreadLocalRandom.current().nextInt(1, 6 + 1); //TODO - assign value to variable?
+  }
+
+  /**
+   * Returns true if "dice roll" is 4,5 or 6 and false otherwise
+   * @return boolean
+   */
+  public boolean rollToTurnMarker(){
+    int number = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+    return number > 3;
   }
 }
