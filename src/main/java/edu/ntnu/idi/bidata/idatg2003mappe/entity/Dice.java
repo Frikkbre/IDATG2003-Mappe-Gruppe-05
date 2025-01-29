dc6f43949fa4bcb1a627057281df1da39991aa04
Frikk Brændsrød <fribra05@yahoo.com>
2025-01-29 12:45:15 +0100

    Added field and method rollDice to Dice class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Dice.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Dice.java
@@ -1,4 +1,14 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.entity;
 
+//Imports
+import java.util.concurrent.ThreadLocalRandom;
+
+
 public class Dice {
+  int number;
+
+  public int rollDice(){
+    int randomInt = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+    return randomInt;
+  }
 }
