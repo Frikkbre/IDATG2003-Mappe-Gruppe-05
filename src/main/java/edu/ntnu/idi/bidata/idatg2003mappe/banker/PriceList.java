18076c96cae04f4c5236ebf5340f743ba07a1011
Frikk Brændsrød <fribra05@yahoo.com>
2025-01-29 13:46:47 +0100

    added doc and methods to PriceList class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/PriceList.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/PriceList.java
@@ -1,4 +1,20 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.banker;
 
+import java.util.HashMap;
+
+/**
+ * This class is responsible for holding the prices for different services.
+ * For example flipping a marker or using a plane route.
+ */
 public class PriceList {
+  HashMap <String, Integer> priceRegister = new HashMap; //TODO - Is this class overkill? refactor to Banker class?
+
+  public PriceList(){
+    priceRegister.put("RedGem", 1000); //TODO - Should this be hardcoded or take inn <obj, int>
+    priceRegister.put("GreenGem", 4000); //Originally double the price at certain places.
+    priceRegister.put("YellowGem", 2000);
+  }
+
+  
+
 }
