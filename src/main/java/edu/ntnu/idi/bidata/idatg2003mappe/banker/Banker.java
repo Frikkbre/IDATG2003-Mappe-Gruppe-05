81a54c451e721e34043a4e9440519a08bec6ad61
Frikk Brændsrød <fribra05@yahoo.com>
2025-01-29 13:46:23 +0100

    added doc to Banker class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/Banker.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/Banker.java
@@ -1,4 +1,8 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.banker;
 
+/**
+ *This class is responsible for transactions of currency during the game, aswell as keeping track of players bank acoount.
+ * Each player should start out with 5000 each.
+ */
 public class Banker {
 }
