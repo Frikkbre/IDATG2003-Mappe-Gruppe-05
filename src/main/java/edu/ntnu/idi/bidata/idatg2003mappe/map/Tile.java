39dcfec1ee478d0d383be6cc8bcd9c4581ec4226
SimenGud <simeng2003@gmail.com>
2025-02-15 00:27:19 +0100

    moveToTile method and setters and getters to tile class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Tile.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Tile.java
@@ -1,4 +1,30 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.map;
 
+/**
+ * Represents a tile on the board.
+ */
+
 public class Tile {
+  private int currentTile;
+  private Tile nextTile;
+  private Tile previousTile;
+  private Tile destinationTile;
+
+  public Tile(int currentTile) {
+    this.currentTile = currentTile;
+  }
+
+  public Tile moveToTile (int steps) { //TODO: Not sure if this is the correct implementation. Needs work.
+    Tile nextTile = this;
+    for (int i = 0; i < steps; i++) {
+      nextTile = nextTile.nextTile;
+    }
+    return nextTile;
+  }
+
+  public int getCurrentTile() {
+    return currentTile;
+  }
+
+
 }
