264b6cdab471b404e9b9e82e52cc15f7782c2b95
SimenGud <simeng2003@gmail.com>
2025-02-16 23:23:57 +0100

    Added addTile to BoardLinear class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
@@ -4,26 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BoardLinear {
+  private List<Tile> tiles;
+  private Tile startTile;
 
-  /*
-  public List<Tile> tiles;
-
-  public void LadderBoard (int totalTiles) {
-    this.tiles = new ArrayList<Tile>();
-    for (int i = 1; i <= totalTiles; i++) {
-      Tile tile = new Tile(i);
-      tiles.add(tile);
-      if (i > 1) {
-        tiles.get(i - 2).addTile(tile);
-        tile.setPreviousTile(tiles.get(i - 2));
-      }
+  public void addTile(Tile tile) {
+    tiles.add(tile);
+    if (tiles.size() == 1) {
+      startTile = tile;
     }
   }
 
-  public Tile getTile(int tileNumber) {
-    return tiles.get(tileNumber - 1);
+  public Tile getStartTile() {
+    return startTile;
   }
-
-   */
-
 }
