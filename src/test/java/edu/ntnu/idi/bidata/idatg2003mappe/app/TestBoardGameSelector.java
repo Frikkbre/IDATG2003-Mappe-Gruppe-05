3e52d59a512342d96f9706ade17dc3ffcf34f771
SimenGud <simeng2003@gmail.com>
2025-02-16 20:05:17 +0100

    Refactored test directory

--- src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/app/TestBoardGameSelector.java
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/app/TestBoardGameSelector.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.bidata.idatg2003mappe;
+package edu.ntnu.idi.bidata.idatg2003mappe.app;
 import edu.ntnu.idi.bidata.idatg2003mappe.app.BoardGameSelector;
 import org.junit.jupiter.api.Test;
 
