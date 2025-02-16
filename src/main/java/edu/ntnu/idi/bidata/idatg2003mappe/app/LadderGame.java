11cda51ab74cabe8ffdf7aa01661afadd1166e1d
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-16 18:47:18 +0100

    Simple print to confirm amount of players and which game was selected in BoardGameSelector

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/LadderGame.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/LadderGame.java
@@ -1,4 +1,8 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
 public class LadderGame {
+
+  public LadderGame(int numberOfPlayers){
+    System.out.println("Starting Ladder Game with " + numberOfPlayers + " players.");
+  }
 }
--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/MissingDiamond.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/MissingDiamond.java
@@ -1,4 +1,8 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
 public class MissingDiamond {
+
+  public MissingDiamond(int numberOfPlayers){
+    System.out.println("Starting Missing Diamond Game with " + numberOfPlayers + " players.");
+  }
 }
