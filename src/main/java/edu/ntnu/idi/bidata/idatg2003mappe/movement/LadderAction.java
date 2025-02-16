2e8cea69b4644850e0d08658dac7e13f57578c8f
SimenGud <simeng2003@gmail.com>
2025-02-16 22:52:49 +0100

    Added goUp and goDown methods to Ladderaction

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/LadderAction.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/LadderAction.java
@@ -1,8 +1,11 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.movement;
 
 public class LadderAction implements TileAction {
-    public void move() {
-        // Move the player up the ladder
+
+    public void goUp() {
+    }
+
+    public void goDown() {
     }
 
   @Override
