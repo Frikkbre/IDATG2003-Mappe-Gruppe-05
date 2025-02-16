10bff20eff0248ee41abd4ff90e2ac506f6465e0
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-16 19:09:36 +0100

    Imported necessary JUnit libraries

--- src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/TestBoardGameSelector.java
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/TestBoardGameSelector.java
@@ -1,4 +1,9 @@
 package edu.ntnu.idi.bidata.idatg2003mappe;
+import edu.ntnu.idi.bidata.idatg2003mappe.app.BoardGameSelector;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBoardGameSelector {
+
 }
