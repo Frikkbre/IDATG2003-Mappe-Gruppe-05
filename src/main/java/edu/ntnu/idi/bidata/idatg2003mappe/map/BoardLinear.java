44f6de388f0f77b56a075299cf32f671bb9cab97
SimenGud <simeng2003@gmail.com>
2025-02-16 22:56:24 +0100

    Setup starting methods to BoardLinear class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
@@ -1,4 +1,26 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardLinear {
+
+  public List<Tile> tiles;
+
+  public void LadderBoard (int totalTiles) {
+    this.tiles = new ArrayList<Tile>();
+    for (int i = 1; i <= totalTiles; i++) {
+      Tile tile = new Tile(i);
+      tiles.add(tile);
+      if (i > 1) {
+        tiles.get(i - 2).addTile(tile);
+        tile.setPreviousTile(tiles.get(i - 2));
+      }
+    }
+  }
+
+  public Tile getTile(int tileNumber) {
+    return tiles.get(tileNumber - 1);
+  }
+
 }
