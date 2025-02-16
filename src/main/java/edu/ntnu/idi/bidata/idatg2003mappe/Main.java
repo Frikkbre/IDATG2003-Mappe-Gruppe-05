59a14d597484c8ef8a2c282c183cac3a7e0a7673
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-16 18:14:38 +0100

    Starting BoardGameSelector in main class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Main.java
@@ -1,9 +1,11 @@
 package edu.ntnu.idi.bidata.idatg2003mappe;
 
+import edu.ntnu.idi.bidata.idatg2003mappe.app.BoardGameSelector;
+
 public class Main {
 
 
   public static void main(String[] args) {
-
+    BoardGameSelector boardGameSelector = new BoardGameSelector();
   }
 }
