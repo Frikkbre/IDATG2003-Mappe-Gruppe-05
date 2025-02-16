106bc2faed996ec020575f36de2365d564550c47
SimenGud <simeng2003@gmail.com>
2025-02-16 20:01:13 +0100

    Added setter and getters to tile class, refactored moveToTile method

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Tile.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Tile.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.map;
 
+import edu.ntnu.idi.bidata.idatg2003mappe.movement.LadderAction;
+
 /**
  * Represents a tile on the board.
  *
@@ -23,16 +25,49 @@ public class Tile {
     this.currentTile = currentTile;
   }
 
+  /**
+   * Moves the player to the next tile.
+   * @param steps The number of steps to move.
+   * @return The next tile.
+   */
+
   public Tile moveToTile (int steps) { //TODO: Not sure if this is the correct implementation. Needs work.
-    Tile nextTile = this;
+    Tile currentTile = this;
     for (int i = 0; i < steps; i++) {
-      nextTile = nextTile.nextTile;
+      if (currentTile.nextTile == null) {
+        return currentTile;
+      }
+      currentTile = currentTile.nextTile;
     }
+    return currentTile;
+  }
+
+  public void setNextTile(Tile nextTile) {
+    this.nextTile = nextTile;
+  }
+
+  public void setPreviousTile(Tile previousTile) {
+    this.previousTile = previousTile;
+  }
+
+  public void setDestinationTile(Tile destinationTile) {
+    this.destinationTile = destinationTile;
+  }
+
+  public int getTile() {
+    return currentTile;
+  }
+
+  public Tile getNextTile() {
     return nextTile;
   }
 
-  public int getCurrentTile() {
-    return currentTile;
+  public Tile getPreviousTile() {
+    return previousTile;
+  }
+
+  public Tile getDestinationTile() {
+    return destinationTile;
   }
 
 
