cc026967442c862cfc4336556edc9bacd993c62d
SimenGud <simeng2003@gmail.com>
2025-02-17 00:06:17 +0100

    Added Testing for tile class

--- src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Tile.java
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Tile.java
@@ -1,34 +1,65 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.map;
 
-import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class Tile {
+class TileTest {
 
   private Tile tile1;
   private Tile tile2;
+  private Tile tile3;
 
   @BeforeEach
-  void setUp() {
-    tile1 = new Tile();
-    tile2 = new Tile();
+  void setup() {
+    tile1 = new Tile(1);
+    tile2 = new Tile(2);
+    tile3 = new Tile(3);
   }
 
-  @AfterEach
-  void tearDown() {
-    tile1 = new Tile(null);
-    tile2 = new Tile(null);
+  @Test void setNextTile() {
+    tile1.setNextTile(tile2);
+    assertEquals(tile2, tile1.getNextTile());
+  }
+
+  @Test void negativeSetNextTile() {
+    tile1.setNextTile(tile2);
+    assertNotEquals(tile1, tile1.getNextTile());
+  }
+
+  @Test void setPreviousTile() {
+    tile2.setPreviousTile(tile1);
+    assertEquals(tile1, tile2.getPreviousTile());
+  }
+
+  @Test void negativeSetPreviousTile() {
+    tile2.setPreviousTile(tile1);
+    assertNotEquals(tile2, tile2.getPreviousTile());
   }
 
   @Test
-  void testSetNextTile() {
-
-
-
+  void testConstructorAndGetTileId() {
+    assertEquals(1, tile1.getTileId());
+    assertEquals(2, tile2.getTileId());
+    assertEquals(3, tile3.getTileId());
   }
 
+  @Test
+  void testSetAndGetNextTile() {
+    tile1.setNextTile(tile2);
+    assertEquals(tile2, tile1.getNextTile());
+  }
 
+  @Test
+  void testSetAndGetPreviousTile() {
+    tile2.setPreviousTile(tile1);
+    assertEquals(tile1, tile2.getPreviousTile());
+  }
 
-
+  @Test
+  void testSetAndGetDestinationTile() {
+    tile1.setDestinationTile(tile3);
+    assertEquals(tile3, tile1.getDestinationTile());
+  }
 }
