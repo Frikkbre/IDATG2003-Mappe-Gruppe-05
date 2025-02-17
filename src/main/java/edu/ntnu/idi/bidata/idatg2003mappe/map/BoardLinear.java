601b6594b3d51e73f4328b44df2f38edd0e8a2a3
SimenGud <simeng2003@gmail.com>
2025-02-17 01:21:44 +0100

    Added Tests to BoardLinear

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
@@ -8,10 +8,14 @@ public class BoardLinear {
   private Tile startTile;
 
   public void addTile(Tile tile) {
+    if (tiles == null) {
+      tiles = new ArrayList<>();
+    }
     tiles.add(tile);
     if (tiles.size() == 1) {
       startTile = tile;
     }
+    System.out.println("Tile added: " + tile);
   }
 
   public Tile getStartTile() {
--- src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
@@ -6,11 +6,39 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class BoardLinear {
+class BoardLinearTest {
+
+  private BoardLinear board;
 
   @BeforeEach
+  void setUp() {
+    board = new BoardLinear();
+  }
 
   @AfterEach
+  void tearDown() {
+    board = null;
+  }
 
+  @Test
+  void testAddTile() {
+    Tile tile = new Tile(1);
+    board.addTile(tile);
+    assertEquals(tile, board.getStartTile(), "Start tile should be the first tile added.");
+  }
+
+  @Test
+  void testAddMultipleTiles() {
+    Tile tile1 = new Tile(1);
+    Tile tile2 = new Tile(2);
+    board.addTile(tile1);
+    board.addTile(tile2);
+    assertEquals(tile1, board.getStartTile(), "Start tile should be the first tile added.");
+  }
+
+  @Test
+  void testGetStartTileWhenEmpty() {
+    assertNull(board.getStartTile(), "Start tile should be null when no tile is added.");
+  }
 
 }
