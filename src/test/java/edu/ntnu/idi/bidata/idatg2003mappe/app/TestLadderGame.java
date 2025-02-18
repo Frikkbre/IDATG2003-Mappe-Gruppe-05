37ecc826a4b6f184005d57e4613be7c54d66d8eb
SimenGud <simeng2003@gmail.com>
2025-02-18 02:31:17 +0100

    Removed unnessesary imports

--- src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/app/TestLadderGame.java
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/app/TestLadderGame.java
@@ -2,13 +2,9 @@ package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import edu.ntnu.idi.bidata.idatg2003mappe.entity.Player;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Field;
-import java.util.List;
 
 public class TestLadderGame {
 
