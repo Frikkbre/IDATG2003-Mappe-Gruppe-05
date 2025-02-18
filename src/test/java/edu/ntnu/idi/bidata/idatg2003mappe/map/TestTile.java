e7d969844177c7bdca7e003cb26b79caa7bf6d10
SimenGud <simeng2003@gmail.com>
2025-02-18 02:09:13 +0100

    Refactored map testing classes

--- src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/TestTile.java
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/TestTile.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TileTest {
+class TestTile {
 
   private Tile tile1;
   private Tile tile2;
