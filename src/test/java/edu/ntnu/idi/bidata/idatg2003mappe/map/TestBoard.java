7212add2d25e7b559554640836e51b489b095f63
SimenGud <simeng2003@gmail.com>
2025-02-22 17:19:42 +0100

    Added testing classes to all classes that need coverage for now

---
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/map/TestBoard.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idi.bidata.idatg2003mappe.map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestBoard {
+}
---
+++ src/test/java/edu/ntnu/idi/bidata/idatg2003mappe/markers/TestMarker.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idi.bidata.idatg2003mappe.markers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestMarker {
+}
