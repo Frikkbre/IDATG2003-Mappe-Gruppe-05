f968ac0086256a69e9a892ac0c2288ccf2033876
SimenGud <simeng2003@gmail.com>
2025-02-22 02:43:36 +0100

    Made BoardBranching class

---
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardBranching.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.bidata.idatg2003mappe.map;
+
+/**
+ * This class represents a board which uses branching paths.
+ * For example Missing Diamond game.
+ *
+ * @version 0.0.1
+ * @since 22.02.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ */
+
+public class BoardBranching {
+}
