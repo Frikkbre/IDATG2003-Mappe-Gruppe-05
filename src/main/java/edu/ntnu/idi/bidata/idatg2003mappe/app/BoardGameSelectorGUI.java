22e7ce5261afc2c7538822696995a2b2c3bb2179
Frikk Brændsrød <fribra05@yahoo.com>
2025-02-22 14:30:11 +0100

    Added screen with two buttons to select game, locked in 16:9 aspect ratio

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorGUI.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorGUI.java
@@ -1,4 +1,46 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
-
-public class BoardGameSelectorGUI {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+public class BoardGameSelectorGUI extends Application {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    BorderPane borderPane = new BorderPane();
+    borderPane.setMinHeight(540); //16:9 apsect ratio   (1920x1080)/2
+    borderPane.setMaxHeight(540);
+    borderPane.setMinWidth(960);
+    borderPane.setMaxWidth(960);
+    borderPane.setPrefHeight(540);
+    borderPane.setPrefWidth(960 );
+    borderPane.setCenter(createCenterPane());
+    borderPane.setTop(createMenuBar());
+    Scene scene = new Scene(borderPane);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Select a board game");
+    primaryStage.show();
+  }
+  private MenuBar createMenuBar() {
+    MenuItem openMenuItem = new MenuItem("Open");
+    MenuItem saveMenuItem = new MenuItem("Save");
+    MenuItem closeMenuItem = new MenuItem("Close");
+    Menu fileMenu = new Menu("File");
+    fileMenu.getItems().addAll(openMenuItem, saveMenuItem, new SeparatorMenuItem(), closeMenuItem);
+    MenuBar menuBar = new MenuBar();
+    menuBar.getMenus().addAll(fileMenu);
+    return menuBar;
+  }
+  private Pane createCenterPane() {
+    Button button1 = new Button("Ladder game");
+    Button button2 = new Button("Missing diamond");
+    FlowPane centerPane = new FlowPane();
+    centerPane.getChildren().addAll(button1, button2);
+    centerPane.setAlignment(Pos.CENTER);
+    return centerPane;
+  }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
