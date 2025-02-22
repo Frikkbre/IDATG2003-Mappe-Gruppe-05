40c656e378a140e9f80861c19ca281d6f1db6c08
SimenGud <simeng2003@gmail.com>
2025-02-22 02:50:24 +0100

    Updated docs for TileAction

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/TileAction.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/TileAction.java
@@ -2,6 +2,14 @@ package edu.ntnu.idi.bidata.idatg2003mappe.movement;
 
 import edu.ntnu.idi.bidata.idatg2003mappe.entity.Player;
 
+/**
+ * Represents a TileAction.
+ *
+ * @version 0.0.1
+ * @since 20.02.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ */
+
 public interface TileAction {
   void performAction(Player player);
 }
