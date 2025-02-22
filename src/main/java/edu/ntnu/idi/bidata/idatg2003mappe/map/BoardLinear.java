83554fd8980617aba0bca0c8076b5bb5d3e8521d
SimenGud <simeng2003@gmail.com>
2025-02-22 03:01:38 +0100

    Added additional BoardLinear class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
@@ -11,9 +11,15 @@ import java.util.List;
  * @author Simen Gudbrandsen and Frikk Breadsroed
  */
 
-public class BoardLinear {
+public class BoardLinear { //TODO: Might be better to make a superclass for BoardLinear. Maybe Board class?
   private List<Tile> tiles;
   private Tile startTile;
+
+  /**
+   * Adds a tile to the board.
+   *
+   * @param tile The tile to add.
+   */
 
   public void addTileToBoard(Tile tile) {
     if (tiles == null) { //Can this ever be null?
@@ -25,15 +31,35 @@ public class BoardLinear {
     }
   }
 
+  /**
+   * Adds multiple tiles to the board.
+   *
+   * @param tiles The tiles to add.
+   */
+
   public void addMultipleTilesToBoard(Tile... tiles) {
     for (Tile tile : tiles) {
       addTileToBoard(tile);
     }
   }
 
+  //TODO: Setters?
+
+  /**
+   * Gets the start tile of the board.
+   *
+   * @return The start tile.
+   */
+
   public Tile getStartTile() {
     return startTile;
   }
+
+  /**
+   * Gets the tiles of the board.
+   *
+   * @return The tiles.
+   */
 
   public List<Tile> getTiles() {
     return tiles;
