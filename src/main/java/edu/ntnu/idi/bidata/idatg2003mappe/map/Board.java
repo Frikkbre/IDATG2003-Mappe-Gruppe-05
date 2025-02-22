48e8f8477d7b3d5cc501c6d00cc4f7ea3ce81ee3
SimenGud <simeng2003@gmail.com>
2025-02-22 16:30:49 +0100

    Moved some BoardLinear methods to a Board Superclass, made a BoardSquare class for future

---
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/Board.java
@@ -0,0 +1,59 @@
+package edu.ntnu.idi.bidata.idatg2003mappe.map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+  protected Tile startTile;
+  protected List<Tile> tiles;
+  protected String boardName;
+
+  /**
+   * Adds a tile to the board.
+   *
+   * @param tile The tile to add.
+   */
+
+  public void addTileToBoard(Tile tile) {
+    if (tiles == null) {
+      tiles = new ArrayList<>();
+    }
+    tiles.add(tile);
+    if (tiles.size() == 1) {
+      startTile = tile;
+    }
+  }
+
+  /**
+   * Adds multiple tiles to the board.
+   *
+   * @param tiles The tiles to add.
+   */
+
+  public void addMultipleTilesToBoard(Tile... tiles) {
+    for (Tile tile : tiles) {
+      addTileToBoard(tile);
+    }
+  }
+
+  /**
+   * Gets the start tile of the board.
+   *
+   * @return The start tile.
+   */
+
+  public Tile getStartTile() {
+    return startTile;
+  }
+
+  /**
+   * Gets the name of the board.
+   *
+   * @return The name of the board.
+   */
+
+  public String getBoardName() {
+    return boardName;
+  }
+
+}
--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/map/BoardLinear.java
@@ -1,6 +1,5 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.map;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,48 +10,10 @@ import java.util.List;
  * @author Simen Gudbrandsen and Frikk Breadsroed
  */
 
-public class BoardLinear { //TODO: Might be better to make a superclass for BoardLinear. Maybe Board class?
-  private List<Tile> tiles;
-  private Tile startTile;
+public class BoardLinear extends Board {
 
-  /**
-   * Adds a tile to the board.
-   *
-   * @param tile The tile to add.
-   */
-
-  public void addTileToBoard(Tile tile) {
-    if (tiles == null) { //Can this ever be null?
-      tiles = new ArrayList<>();
-    }
-    tiles.add(tile);
-    if (tiles.size() == 1) {
-      startTile = tile;
-    }
-  }
-
-  /**
-   * Adds multiple tiles to the board.
-   *
-   * @param tiles The tiles to add.
-   */
-
-  public void addMultipleTilesToBoard(Tile... tiles) {
-    for (Tile tile : tiles) {
-      addTileToBoard(tile);
-    }
-  }
-
-  //TODO: Setters?
-
-  /**
-   * Gets the start tile of the board.
-   *
-   * @return The start tile.
-   */
-
-  public Tile getStartTile() {
-    return startTile;
+  public BoardLinear() {
+    super();
   }
 
   /**
