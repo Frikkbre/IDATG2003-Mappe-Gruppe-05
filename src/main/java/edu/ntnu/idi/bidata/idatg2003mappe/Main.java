6d4e2d887beded2c79f3158a9318f32d9bb5cd46
SimenGud <simeng2003@gmail.com>
2025-02-22 02:53:37 +0100

    Added docs for Main class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/Main.java
@@ -2,8 +2,21 @@ package edu.ntnu.idi.bidata.idatg2003mappe;
 
 import edu.ntnu.idi.bidata.idatg2003mappe.app.BoardGameSelector;
 
+/**
+ * Main class for the application.
+ *
+ * @version 0.0.1
+ * @since 16.02.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ */
+
 public class Main {
 
+  /**
+   * Main method for the application.
+   *
+   * @param args Command line arguments
+   */
 
   public static void main(String[] args) {
     BoardGameSelector boardGameSelector = new BoardGameSelector();
