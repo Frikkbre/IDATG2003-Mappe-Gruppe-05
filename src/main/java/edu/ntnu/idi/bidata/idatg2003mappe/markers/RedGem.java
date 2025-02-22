12581882168ed7553d455dcef14c2561aae436dc
SimenGud <simeng2003@gmail.com>
2025-02-22 02:49:03 +0100

    Simple docs for the red gem marker class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/markers/RedGem.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/markers/RedGem.java
@@ -1,4 +1,12 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.markers;
 
+/**
+ * A red gem marker.
+ *
+ * @version 0.0.1
+ * @since 03.02.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ */
+
 public class RedGem {
 }
