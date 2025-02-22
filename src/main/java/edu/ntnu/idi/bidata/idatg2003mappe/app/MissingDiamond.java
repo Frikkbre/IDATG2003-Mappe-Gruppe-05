53d89b5e44af230cbbf2d495adb71e4f0eb76cb1
SimenGud <simeng2003@gmail.com>
2025-02-22 02:51:01 +0100

    Class description docs for MissingDiamond class

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/MissingDiamond.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/MissingDiamond.java
@@ -1,6 +1,20 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
+/**
+ * Represents the Missing Diamond game.
+ *
+ * @version 0.0.1
+ * @since 16.02.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ */
+
 public class MissingDiamond {
+
+  /**
+   * Constructor for the MissingDiamond class.
+   *
+   * @param numberOfPlayers The number of players in the game.
+   */
 
   public MissingDiamond(int numberOfPlayers){
     System.out.println("Starting Missing Diamond Game with " + numberOfPlayers + " players.");
