fa846470bc22b76356802ecbe7d219e67e29e6ec
SimenGud <simeng2003@gmail.com>
2025-02-22 02:49:40 +0100

    Simple docs for Visa and Yellow gem marker classes

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/markers/Visa.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/markers/Visa.java
@@ -1,4 +1,12 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.markers;
 
+/**
+ * Represents a Visa marker.
+ *
+ * @version 0.0.1
+ * @since 03.02.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ */
+
 public class Visa {
 }
