3f045350c26bd74d56cf5b21b352b359963e5a9a
SimenGud <simeng2003@gmail.com>
2025-02-22 02:57:20 +0100

    Added docs to Action class, is deprecated, will remove later

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Action.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Action.java
@@ -1,4 +1,13 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.entity;
 
+/**
+ * Represents an action.
+ *
+ * @version 0.0.1
+ * @since 19.01.2025
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ * @deprecated This class is deprecated and will be removed in the next release.
+ */
+
 public class Action {
 }
