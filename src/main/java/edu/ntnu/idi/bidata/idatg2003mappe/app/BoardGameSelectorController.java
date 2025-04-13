4d8f17e60001835faf0ffdfde103a27912ced01d
Frikk Brændsrød <fribra05@yahoo.com>
2025-04-13 18:52:09 +0200

    Added middle layer for eventhandler and switch game

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorController.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorController.java
@@ -1,4 +1,9 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
 public class BoardGameSelectorController {
+  BoardGameSelector boardGameSelector;
+
+  public void switchGame(int gameSelector) {
+    boardGameSelector.switchGame(gameSelector);
+  }
 }
