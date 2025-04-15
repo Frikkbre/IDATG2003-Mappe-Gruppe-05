df49411b86560fcf55f3232fe1841ba88c83d850
Frikk Brændsrød <fribra05@yahoo.com>
2025-04-15 16:49:04 +0200

    fixed instanciation of boardGameSelector

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorController.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorController.java
@@ -1,7 +1,7 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
 public class BoardGameSelectorController {
-  BoardGameSelector boardGameSelector;
+  BoardGameSelector boardGameSelector = new BoardGameSelector();
 
   public void switchGame(int gameSelector) {
     boardGameSelector.switchGame(gameSelector);
