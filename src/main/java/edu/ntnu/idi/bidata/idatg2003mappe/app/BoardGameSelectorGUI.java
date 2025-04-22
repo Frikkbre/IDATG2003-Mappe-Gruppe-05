0fc301a63518633fc9cb5373853c184b77f9baaa
Frikk Brændsrød <fribra05@yahoo.com>
2025-04-22 18:32:10 +0200

    Added eventhandlers that switch scene to desired game on button press

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorGUI.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameSelectorGUI.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
 import edu.ntnu.idi.bidata.idatg2003mappe.app.laddergame.LadderGameGUI;
+import edu.ntnu.idi.bidata.idatg2003mappe.app.missingdiamond.MissingDiamond;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -15,21 +16,30 @@ import javafx.stage.Stage;
  */
 public class BoardGameSelectorGUI extends Application {
   private LadderGameGUI LadderGame;
+  private Stage primaryStage;
+
+  public Stage getStage() {
+    return primaryStage;
+  }
+    public void setStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
 
   @Override
   public void start(Stage primaryStage) throws Exception {
     BorderPane borderPane = new BorderPane();
-    borderPane.setMinHeight(540); //16:9 aspect ratio   (1920x1080)/2
-    borderPane.setMaxHeight(540);
-    borderPane.setMinWidth(960);
-    borderPane.setMaxWidth(960);
-    borderPane.setPrefHeight(540);
-    borderPane.setPrefWidth(960);
+    borderPane.setMinHeight(840); //16:9 aspect ratio   (1920x1080)/2
+    borderPane.setMaxHeight(840);
+    borderPane.setMinWidth(1440);
+    borderPane.setMaxWidth(1440);
+    borderPane.setPrefHeight(840);
+    borderPane.setPrefWidth(1440);
     borderPane.setCenter(createCenterPane());
 
     NavBar navBar = new NavBar();
     borderPane.setTop(navBar.createMenuBar());
     Scene scene = new Scene(borderPane);
+    setStage(primaryStage);
     primaryStage.setScene(scene);
     primaryStage.setTitle("Select a board game");
     primaryStage.show();
@@ -40,7 +50,21 @@ public class BoardGameSelectorGUI extends Application {
 
   private Pane createCenterPane() {
     Button button1 = new Button("Ladder game");
+    button1.setOnAction(event -> {
+      try {
+        LadderGame.start(getStage());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
     Button button2 = new Button("Missing diamond");
+    button2.setOnAction(event -> {
+      try {
+        //MissingDiamond.start(getStage()); TODO - implement diamond game start method.
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
     FlowPane centerPane = new FlowPane();
     centerPane.getChildren().addAll(button1, button2);
     centerPane.setAlignment(Pos.CENTER);
