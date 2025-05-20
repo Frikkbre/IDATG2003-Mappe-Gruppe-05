572f414f1daf1bd72e2373f416e101927678f391
Frikk Brændsrød <fribra05@yahoo.com>
2025-05-20 17:32:06 +0200

    Refactored GameRegistry to fileHandling package

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/filehandling/game/GameRegistry.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/filehandling/game/GameRegistry.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.bidata.idatg2003mappe.app;
+package edu.ntnu.idi.bidata.idatg2003mappe.filehandling.game;
 
 import edu.ntnu.idi.bidata.idatg2003mappe.app.laddergame.LadderGameController;
 import edu.ntnu.idi.bidata.idatg2003mappe.app.missingdiamond.MissingDiamondController;
