c898a9cb7f8db4d9b7c81b1a543235cfa03683bb
Frikk Brændsrød <fribra05@yahoo.com>
2025-05-21 14:38:27 +0200

    Added switch case to determine which effect is to be applied and added DOC

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/EffectTile.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/EffectTile.java
@@ -4,22 +4,56 @@ import edu.ntnu.idi.bidata.idatg2003mappe.entity.Player;
 import edu.ntnu.idi.bidata.idatg2003mappe.map.Tile;
 
 /**
- * Class that handles effect tiles in Ladder game
- * Has effects like making a player skip a turn or get extra points
+ * Class that handles effect tiles in board games.
+ *
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ * @version 0.0.1
+ * @since 21.05.2025
  */
 public class EffectTile implements TileAction {
 
+  private final Tile currentTile;
+  private final String effectType;
 
-  @Override
-  public void performAction(Player player) {
-    /*if(currentTile.getEffect() != null) {
-      currentTile.getEffect().applyEffect(player);
-    } else {
-      System.out.println("No effect action on this tile.");
-    }*/
-    System.out.println("Effect action: applying effect to player on tile ");
+  /**
+   * Constructor for the EffectTile class.
+   *
+   * @param currentTile The tile where the effect is triggered
+   * @param effectType The type of effect to apply
+   */
+  public EffectTile(Tile currentTile, String effectType) {
+    this.currentTile = currentTile;
+    this.effectType = effectType;
   }
 
+  /**
+   * Performs the effect action on the player based on the effect type.
+   *
+   * @param player The player to apply the effect to
+   */
+  @Override
+  public void performAction(Player player) {
+    if (effectType == null) {
+      System.out.println("No effect action on this tile.");
+      return;
+    }
+
+    switch (effectType) {
+      case "skipTurn":
+        skipTurn(player);
+        System.out.println(player.getName() + " will skip their next turn!");
+        break;
+      default:
+        System.out.println("Unknown effect type: " + effectType);
+        break;
+    }
+  }
+
+  /**
+   * Makes the player skip their next turn.
+   *
+   * @param player The player who will skip their turn
+   */
   public void skipTurn(Player player) {
     player.setSkipTurn(true);
   }
