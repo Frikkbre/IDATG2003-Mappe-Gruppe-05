9dd14f62ac0f1d005a7a9e799031ddee82b2e060
SimenGud <simeng2003@gmail.com>
2025-05-21 11:15:24 +0200

    Refactored Die class and observer interface

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Die.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/Die.java
@@ -1,18 +1,51 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Represents a die.
+ * Represents a die with Observer pattern support.
  *
  * @author Simen Gudbrandsen and Frikk Breadsroed
- * @version 0.0.3
- * @since 03.02.2025
+ * @version 0.0.4
+ * @since 25.05.2025
  */
-
 public class Die {
 
   private int dieValue;
+
+  // Observer pattern support
+  private List<DieObserver> observers = new ArrayList<>();
+
+  /**
+   * Adds an observer to the die.
+   *
+   * @param observer The observer to add.
+   */
+  public void addObserver(DieObserver observer) {
+    observers.add(observer);
+  }
+
+  /**
+   * Removes an observer from the die.
+   *
+   * @param observer The observer to remove.
+   */
+  public void removeObserver(DieObserver observer) {
+    observers.remove(observer);
+  }
+
+  /**
+   * Notifies observers that the die has been rolled.
+   *
+   * @param value The value rolled.
+   */
+  private void notifyDieRolled(int value) {
+    for (DieObserver observer : observers) {
+      observer.onDieRolled(value);
+    }
+  }
 
   /**
    * Return an integer between 1 and 6 (inclusive) to simulate a die.
@@ -21,6 +54,10 @@ public class Die {
    */
   public int rollDie() {
     dieValue = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+
+    // Notify observers about the roll
+    notifyDieRolled(dieValue);
+
     return dieValue;
   }
 
@@ -31,6 +68,10 @@ public class Die {
    */
   public boolean rollToTurnMarker() {
     dieValue = ThreadLocalRandom.current().nextInt(3, 7);
+
+    // Notify observers about the roll
+    notifyDieRolled(dieValue);
+
     return dieValue > 3;
   }
 
---
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/entity/DieObserver.java
@@ -0,0 +1,18 @@
+package edu.ntnu.idi.bidata.idatg2003mappe.entity;
+
+/**
+ * Observer interface for die events.
+ *
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ * @version 0.0.1
+ * @since 21.05.2025
+ */
+public interface DieObserver {
+
+  /**
+   * Called when a die has been rolled.
+   *
+   * @param value The value rolled.
+   */
+  void onDieRolled(int value);
+}
