f88fb306b98b3328331f359fed70e990cd0fc902
SimenGud <simeng2003@gmail.com>
2025-05-21 17:34:34 +0200

    Remade Banker and PriceList

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/Banker.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/Banker.java
@@ -1,5 +1,9 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.banker;
 
+import edu.ntnu.idi.bidata.idatg2003mappe.entity.player.Player;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class is responsible for transactions of currency during the game,
  * as well as keeping track of players bank account.
@@ -9,6 +13,120 @@ package edu.ntnu.idi.bidata.idatg2003mappe.banker;
  * @version 0.0.1
  * @since 03.02.2025
  */
-
 public class Banker {
+  private static final int STARTING_BALANCE = 5000;
+  private final Map<Player, Integer> playerAccounts = new HashMap<>();
+  private final PriceList priceList;
+
+  /**
+   * Constructor for the Banker class.
+   */
+  public Banker() {
+    this.priceList = new PriceList();
+  }
+
+  /**
+   * Registers a player with the banker and gives them their starting balance.
+   *
+   * @param player The player to register
+   */
+  public void registerPlayer(Player player) {
+    playerAccounts.put(player, STARTING_BALANCE);
+  }
+
+  /**
+   * Gets the balance of a player.
+   *
+   * @param player The player to get the balance for
+   * @return The player's balance
+   */
+  public int getBalance(Player player) {
+    return playerAccounts.getOrDefault(player, 0);
+  }
+
+  /**
+   * Deposits money into a player's account.
+   *
+   * @param player The player to deposit money to
+   * @param amount The amount to deposit
+   */
+  public void deposit(Player player, int amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Amount must be positive");
+    }
+
+    int currentBalance = getBalance(player);
+    playerAccounts.put(player, currentBalance + amount);
+  }
+
+  /**
+   * Withdraws money from a player's account.
+   *
+   * @param player The player to withdraw money from
+   * @param amount The amount to withdraw
+   * @return true if the withdrawal was successful, false if the player has insufficient funds
+   */
+  public boolean withdraw(Player player, int amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Amount must be positive");
+    }
+
+    int currentBalance = getBalance(player);
+    if (currentBalance < amount) {
+      return false; // Insufficient funds
+    }
+
+    playerAccounts.put(player, currentBalance - amount);
+    return true;
+  }
+
+  /**
+   * Transfers money from one player to another.
+   *
+   * @param fromPlayer The player to transfer money from
+   * @param toPlayer The player to transfer money to
+   * @param amount The amount to transfer
+   * @return true if the transfer was successful, false if the fromPlayer has insufficient funds
+   */
+  public boolean transfer(Player fromPlayer, Player toPlayer, int amount) {
+    if (withdraw(fromPlayer, amount)) {
+      deposit(toPlayer, amount);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Gets the price for a gem.
+   *
+   * @param gemType The type of gem
+   * @return The price of the gem
+   */
+  public int getGemPrice(String gemType) {
+    return priceList.getPrice(gemType);
+  }
+
+  /**
+   * Checks if a player can afford to buy a gem.
+   *
+   * @param player The player
+   * @param gemType The type of gem
+   * @return true if the player can afford the gem, false otherwise
+   */
+  public boolean canAffordGem(Player player, String gemType) {
+    int price = getGemPrice(gemType);
+    return getBalance(player) >= price;
+  }
+
+  /**
+   * Buys a gem for a player.
+   *
+   * @param player The player
+   * @param gemType The type of gem
+   * @return true if the purchase was successful, false otherwise
+   */
+  public boolean buyGem(Player player, String gemType) {
+    int price = getGemPrice(gemType);
+    return withdraw(player, price);
+  }
 }
--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/PriceList.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/banker/PriceList.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.banker;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class is responsible for holding the prices for different services.
@@ -11,14 +12,50 @@ import java.util.HashMap;
  * @since 07.02.2025
  */
 public class PriceList {
-  HashMap<String, Integer> priceRegister = new HashMap();  //TODO - Is this class overkill? refactor to Banker class?
+  private final Map<String, Integer> priceRegister = new HashMap<>();
 
-
+  /**
+   * Constructor for the PriceList class.
+   * Initializes prices for different services.
+   */
   public PriceList() {
-    priceRegister.put("RedGem", 1000); //TODO - Should this be hardcoded or take inn <obj, int>
-    priceRegister.put("GreenGem", 4000); //Originally double the price at certain places.
+    priceRegister.put("RedGem", 1000);
+    priceRegister.put("GreenGem", 4000);
     priceRegister.put("YellowGem", 2000);
+    priceRegister.put("PlaneTicket", 3000);
+    priceRegister.put("ShipTicket", 1500);
   }
 
+  /**
+   * Gets the price for a service.
+   *
+   * @param service The service to get the price for
+   * @return The price of the service, or 0 if the service is not found
+   */
+  public int getPrice(String service) {
+    return priceRegister.getOrDefault(service, 0);
+  }
 
+  /**
+   * Sets the price for a service.
+   *
+   * @param service The service to set the price for
+   * @param price The new price
+   */
+  public void setPrice(String service, int price) {
+    if (price < 0) {
+      throw new IllegalArgumentException("Price cannot be negative");
+    }
+    priceRegister.put(service, price);
+  }
+
+  /**
+   * Checks if a service exists in the price list.
+   *
+   * @param service The service to check
+   * @return true if the service exists, false otherwise
+   */
+  public boolean hasService(String service) {
+    return priceRegister.containsKey(service);
+  }
 }
