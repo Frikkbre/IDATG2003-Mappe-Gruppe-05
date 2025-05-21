01df18d3590841ef938445e8b83f907ed4d59506
SimenGud <simeng2003@gmail.com>
2025-05-21 17:52:26 +0200

    Added a MissingDiamondAction class

---
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/MissingDiamondAction.java
@@ -0,0 +1,95 @@
+package edu.ntnu.idi.bidata.idatg2003mappe.movement;
+
+import edu.ntnu.idi.bidata.idatg2003mappe.entity.player.Player;
+import edu.ntnu.idi.bidata.idatg2003mappe.map.Tile;
+import edu.ntnu.idi.bidata.idatg2003mappe.markers.Marker;
+
+/**
+ * Action for special tiles in the Missing Diamond game.
+ *
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ * @version 0.1.0
+ * @since 22.05.2025
+ */
+public class MissingDiamondAction implements TileAction {
+  private final Tile currentTile;
+  private final String actionType;
+  private Marker marker;
+
+  /**
+   * Constructor for the MissingDiamondAction class.
+   *
+   * @param currentTile The tile where the action occurs
+   * @param actionType The type of action to perform
+   */
+  public MissingDiamondAction(Tile currentTile, String actionType) {
+    this.currentTile = currentTile;
+    this.actionType = actionType;
+  }
+
+  /**
+   * Constructor for the MissingDiamondAction class with a marker.
+   *
+   * @param currentTile The tile where the action occurs
+   * @param actionType The type of action to perform
+   * @param marker The marker involved in the action
+   */
+  public MissingDiamondAction(Tile currentTile, String actionType, Marker marker) {
+    this.currentTile = currentTile;
+    this.actionType = actionType;
+    this.marker = marker;
+  }
+
+  @Override
+  public void performAction(Player player) {
+    switch (actionType) {
+      case "RevealMarker":
+        if (marker != null) {
+          marker.reveal();
+        }
+        break;
+      case "PurchaseMarker":
+        // This would require the banker for actual implementation
+        break;
+      case "Transport":
+        // Move the player to a connected tile (plane/ship)
+        break;
+      case "Diamond":
+        // Found the diamond - win the game
+        break;
+      case "Bandit":
+        // Bandit takes all your markers
+        break;
+      default:
+        System.out.println("Unknown action type: " + actionType);
+        break;
+    }
+  }
+
+  /**
+   * Gets the current tile for this action.
+   *
+   * @return The current tile
+   */
+  public Tile getCurrentTile() {
+    return currentTile;
+  }
+
+  /**
+   * Gets the action type.
+   *
+   * @return The action type
+   */
+  public String getActionType() {
+    return actionType;
+  }
+
+  /**
+   * Gets the marker for this action.
+   *
+   * @return The marker
+   */
+  public Marker getMarker() {
+    return marker;
+  }
+}
