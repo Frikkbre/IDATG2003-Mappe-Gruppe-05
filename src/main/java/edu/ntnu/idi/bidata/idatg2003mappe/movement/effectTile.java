797212968f49457ac60de61e0d982c6818ef8982
Frikk Brændsrød <fribra05@yahoo.com>
2025-05-21 14:20:21 +0200

    Made class to handle effect tiles

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/effectTile.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/movement/effectTile.java
@@ -1,4 +1,31 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.movement;
 
-public class effectTile {
+import edu.ntnu.idi.bidata.idatg2003mappe.entity.Player;
+import edu.ntnu.idi.bidata.idatg2003mappe.map.Tile;
+
+/**
+ * Class that handles effect tiles in Ladder game
+ * Has effects like making a player skip a turn or get extra points
+ */
+public class effectTile implements TileAction {
+
+  private final Tile currentTile;
+
+  public effectTile(Tile currentTile) {
+    this.currentTile = currentTile;
+
+  }
+
+  @Override
+  public void performAction(Player player) {
+    if(currentTile.getEffect() != null) {
+      currentTile.getEffect().applyEffect(player);
+    } else {
+      System.out.println("No effect action on this tile.");
+    }
+  }
+
+  public void skipTurn(Player player) {
+    player.setSkipTurn(true);
+  }
 }
