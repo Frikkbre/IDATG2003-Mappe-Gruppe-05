9aa5beca9c3794f799b120c2a44c077cef017b7b
SimenGud <simeng2003@gmail.com>
2025-05-21 11:04:07 +0200

    Added BoardGameObserver Interface

--- src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameObserver.java
+++ src/main/java/edu/ntnu/idi/bidata/idatg2003mappe/app/BoardGameObserver.java
@@ -1,4 +1,45 @@
 package edu.ntnu.idi.bidata.idatg2003mappe.app;
 
-public class BoardGameObserver {
+import edu.ntnu.idi.bidata.idatg2003mappe.entity.Player;
+import edu.ntnu.idi.bidata.idatg2003mappe.map.Tile;
+
+/**
+ * Observer interface for board game events.
+ *
+ * @author Simen Gudbrandsen and Frikk Breadsroed
+ * @version 0.0.1
+ * @since 21.05.2025
+ */
+public interface BoardGameObserver {
+
+  /**
+   * Called when a player has moved.
+   *
+   * @param player The player who moved.
+   * @param fromTile The tile the player moved from.
+   * @param toTile The tile the player moved to.
+   */
+  void onPlayerMoved(Player player, Tile fromTile, Tile toTile);
+
+  /**
+   * Called when a die has been rolled.
+   *
+   * @param player The player who rolled.
+   * @param rollValue The value rolled.
+   */
+  void onDieRolled(Player player, int rollValue);
+
+  /**
+   * Called when the game has ended.
+   *
+   * @param winner The winning player.
+   */
+  void onGameEnded(Player winner);
+
+  /**
+   * Called when the turn changes to a new player.
+   *
+   * @param newCurrentPlayer The new current player.
+   */
+  void onTurnChanged(Player newCurrentPlayer);
 }
